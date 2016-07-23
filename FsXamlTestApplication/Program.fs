364c3f984c24e9abb10a6abf8bdc58a928895628
Jan Schiefer <jschiefer@users.noreply.github.com>
2016-07-22 21:16:06 -0700

    Migrate initial F# sample from branch in the Avalonia repo

---
+++ FsXamlTestApplication/Program.fs
@@ -0,0 +1,21 @@
+﻿open System
+open System.Windows.Threading
+open Serilog
+open Avalonia.Logging.Serilog
+open Avalonia.Controls
+open XamlTestApplication
+
+let InitializeLogging() =
+#if DEBUG 
+    let loggerConfig = new LoggerConfiguration()
+    let logger = loggerConfig.MinimumLevel.Warning().WriteTo.Trace(outputTemplate = "{Area}: {Message}").CreateLogger()
+    SerilogLogger.Initialize(logger)
+#endif
+    
+    ()
+
+[<EntryPoint>]
+let main argv = 
+    let foo = Dispatcher.CurrentDispatcher
+    AppBuilder.Configure<XamlTestApp>().UsePlatformDetect().Start<Views.MainWindow>()
+    0 // return an integer exit code
